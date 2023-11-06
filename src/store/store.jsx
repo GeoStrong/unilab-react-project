6a7585e3261aa3d1f49c065cc6ff774171c43288
geotrong <dzobavageorgij@gmail.com>
2023-11-06 22:43:29 +0400

    refactoring store

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,24 +1,11 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-
-const initialState = {
-  data: null,
-};
-
-const accountSlice = createSlice({
-  name: 'account',
-  initialState,
-  reducers: {
-    addDataHandler: (state, action) => {
-      const { profile, name } = action.payload;
-      state.data = { profile, name };
-    },
-  },
-});
+import { configureStore } from '@reduxjs/toolkit';
+import dataFilterReducer from './dataFilter';
+import popupReducer from './popup';
 
 const store = configureStore({
   reducer: {
-    account: accountSlice.reducer,
+    dataFilter: dataFilterReducer,
+    popup: popupReducer,
   },
 });
-export const accountActions = accountSlice.actions;
 export default store;
