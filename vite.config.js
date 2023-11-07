7948a45ddd4baa40b0860dc06d18183f569c1fea
geotrong <dzobavageorgij@gmail.com>
2023-11-07 19:24:39 +0400

    publishing to gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/unilab-react-project',
   plugins: [react()],
   css: {
     preprocessorOptions: {
