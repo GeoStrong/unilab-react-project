bd09ab5727e0b2e46fa5abe5535efdbe5496d3d2
geotrong <dzobavageorgij@gmail.com>
2023-11-07 18:56:10 +0400

    profile popup

--- src/pages/Route.jsx
+++ src/pages/Route.jsx
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import ProfilePopup from '../components/Popup/ProfilePopup/ProfilePopup';
 
 const RouteLayout = () => {
+  const { profilePopup } = useSelector((state) => state.popup);
+
   return (
     <>
       <Outlet />
+      {profilePopup && <ProfilePopup />}
     </>
   );
 };
